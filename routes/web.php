972c553ef624ad84f0be972ce27137fdc71bd148
kubrakilic-git <123.harran.kubra@gmail.com>
2020-08-09 11:40:52 +0300

    kategori sayfası

--- routes/web.php
+++ routes/web.php
@@ -3,8 +3,8 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/','AnasayfaController@index')->name('anasayfa');
+Route::get('/kategori/{slug_kategoriadi}','KategoriController@index')->name('kategori');
 
-Route::view('/kategori','kategori');
 Route::view('/urun','urun');
 Route::view('/sepet','sepet');
 
