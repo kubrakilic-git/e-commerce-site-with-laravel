04a33749efcca5cf0efc2c6ed1f0e41b441d96f3
kubrakilic-git <123.harran.kubra@gmail.com>
2020-08-14 12:17:46 +0300

    kullanıcı sayfalarını oluşturma

--- routes/web.php
+++ routes/web.php
@@ -9,4 +9,7 @@ Route::get('/sepet','SepetController@index')->name('sepet');
 Route::get('/odeme','OdemeController@index')->name('odeme');
 Route::get('/siparisler','SiparisController@index')->name('siparisler');
 Route::get('/siparisler/{id}','SiparisController@detay')->name('siparis');
+Route::get('/kullanici/oturumac','KullaniciController@giris_form')->name('kullanici.oturumac');
+Route::get('/kullanici/kaydol','KullaniciController@kaydol_form')->name('kullanici.kaydol');
+
 
