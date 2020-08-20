daa0408f344cfc8d53f7a087b1988689a42de6fe
Kübra KILIÇ <57139092+kubrakilic-git@users.noreply.github.com>
2020-08-20 21:20:54 +0300

    kategori yapısı oluşturuldu

--- app/Http/Controllers/AnasayfaController.php
+++ app/Http/Controllers/AnasayfaController.php
@@ -2,11 +2,13 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Kategori;
 use Illuminate\Http\Request;
 
 class AnasayfaController extends Controller
 {
     public function index(){
-        return view('anasayfa');
+        $kategoriler = Kategori::whereRaw('ust_id is null')->take(8)->get();
+        return view('anasayfa',compact('kategoriler'));
     }
 }
--- app/Models/Kategori.php
+++ app/Models/Kategori.php
@@ -3,9 +3,16 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Kategori extends Model
 {
-    protected $table = 'kategori';
-    public $timestamps = false;
+    use SoftDeletes;
+
+    protected $table = "kategori";
+   // protected $fillable = ['kategori_adi','slug'];
+    protected $guarded = [];
+    const CREATED_AT = 'olusturulma_tarihi';
+    const UPDATED_AT = 'guncelleme_tarihi';
+    const DELETED_AT = 'silinme_tarihi';
 }
